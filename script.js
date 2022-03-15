5e125d007e9b8a6e147d62b9552ac107bcf009c0
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-03-14 22:08:32 -0500

    Time blocks created.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var timeDisplayEl = $("#currentDay")
-
+var dayDisplayEl = $("#currentDay")
+var nineBlockEl = $(" .nineBlockDiv")
 // get the current time when the page loads
 //as you loop through the time blocks in the calendar, compare that time with the current time.
 //That will tell you how to color code each of the blocks.
@@ -7,11 +7,22 @@ var timeDisplayEl = $("#currentDay")
 // moment.js Query - is before, is same, is after
 
 //create a function to display the current time as the page loads
-function displayTime (){
+function displayDay (){
     // utilize moment.js to display time
-    var curretTime = moment().format("MMMM Do YYYY");
-    timeDisplayEl.text(curretTime)
+    var currentTime = moment().format("MMMM Do YYYY");
+    dayDisplayEl.text(currentTime)
 }
 
-//call the display time function and set seconds in milliseconds
-setInterval(displayTime(), 1000)
+//create a function to determine if the time is in the past. We can do this by adding classes .past .present .future
+// function tenseTime(
+    //for loop that will loop through all of the blocks of time on our planner
+    // if statements if( current time === current time )
+        //add .present to our class list.
+// )
+//create a function that when the save button is hit, the text is saved in the corresponding box
+// it will need to be saved to Local Storage. When I refresh the page, the saved text needs to stay where it is.
+
+//call the display time function
+displayDay()
+
+//name the object variables that we will be working with
